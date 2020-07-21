7e5ac659663cb8f1e8b32b96c118c5aeeab91c19
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-07-21 14:03:15 +0530

    Task add complete

--- cmd/add.go
+++ cmd/add.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"strings"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -13,6 +14,11 @@ var addCmd = &cobra.Command{
 	Short: "Add a task to your lists of tasks",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("Something wentwrong, ", err.Error())
+			return
+		}
 		fmt.Printf("Added %s to your task list\n", task)
 	},
 }
